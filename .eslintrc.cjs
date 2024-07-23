cfaaa49d36aadcba7888923ad96d00f5a8c0dcb9
Dave Welsh <dave@qccareerschool.com>
2024-07-23 11:08:20 -0400

    Added plugin:react/jsx-runtime

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:import/recommended',
     'plugin:import/typescript',
   ],
