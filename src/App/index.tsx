a1abe858ecf3c578af0bf33226a68d4c1fda36ab
Dave Welsh <dave@qccareerschool.com>
2024-07-23 11:34:47 -0400

    Added instructions

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,5 +1,6 @@
 import type { FC } from 'react';
 
+import { Parent } from '../State';
 import { users } from '../users';
 import { Footer } from './Footer';
 import { Header } from './Header';
@@ -20,7 +21,8 @@ export const App: FC = () => (
       <Section>
         <div className="container">
           <h2>User List</h2>
-          &lt;insert table here&gt;
+          <p>Create a new component called UsersTable that accepts an array of users as a prop and displays the users in a table. Add the component below and pass in the users as a prop.</p>
+          &lt;insert UsersTable here&gt;
           <pre>
             {JSON.stringify(users, null, '  ')}
           </pre>
@@ -29,7 +31,16 @@ export const App: FC = () => (
       <Section>
         <div className="container">
           <h2>Counter</h2>
-          &lt;insert counter here&gt;
+          <p>Create a new component called Counter that displays a counter with an initial value of 0. Increment the counter by one once per second. Include a button in the component that, when clicked, increments the counter by one. Add the component below.</p>
+          &lt;insert Counter here&gt;
+        </div>
+      </Section>
+      <Section>
+        <div className="container">
+          <h2>State</h2>
+          <p>Create a new component called Parent that displays a message, where the message's initial value is passed in as a prop. Add the component below and pass in an initial message of &quot;Hello&quot;.</p>
+          <p>Create a second component called Child. Add the component to Parent. Add a button to Child that, when clicked, changes the message to &quot;Hello, World!&quot;.</p>
+          &lt;insert Parent here&gt;
         </div>
       </Section>
     </Main>
